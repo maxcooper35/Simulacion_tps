11951ab4a684cc1116ed0b291c8be573b8cd4bf8
Gino Bartolucci <ginobartolucci00@gmail.com>
2024-03-28 20:38:19 -0300

    frecuencias

--- Trabajos_Practicos/Simulador ruleta.py
+++ Trabajos_Practicos/Simulador ruleta.py
@@ -1,53 +1,32 @@
+import json
 import random
-import sys
+import argparse
 
-# if len(sys.argv) != 3 or sys.argv[1] != "-n":
-#     print("Uso: python programa.py -n <num_valores>")
-#     sys.exit(1)
+parser = argparse.ArgumentParser(description="Simulador de ruleta")
+parser.add_argument("-c", default=50, type=int, help="Número de tiradas")
+parser.add_argument("-n", default=1, type=int, help="Número de corridas")
+parser.add_argument("-e", required=True, type=int, choices=range(0, 37), help="Número elegido")
 
+args = parser.parse_args()
 
-# Obtener el número de valores de los argumentos de la línea de comandos
-# num_tiradas = int(sys.argv[0])
-# num_corridas = int(sys.argv[1])
-
-num_tiradas = 100
-num_corridas = 5
-numero = int(input('ingrese numero: '))
+num_tiradas = args.c
+num_corridas = args.n
+numero = args.e
 
 frecuencias={}
 for y in range(num_corridas):
-    valores = [random.randint(0, 36) for _ in range(num_tiradas)]
+    valores = []
+    for c in range(1,num_tiradas + 1 ,1):
+        valores.append(random.randint(0, 36))
+        #desvicion y varianza no se si estan bien me lo tiro el copilot
+        frecuencias[c] = {"frecuencia relativa": round((valores.count(numero) / c),4), 
+                           "valor promedio de las tiradas": round((sum(valores) / len(valores)),4),
+                           "desviacion estandar": round(((sum([(x - sum(valores) / len(valores))**2 for x in valores]) / len(valores))**0.5),4),
+                           "varianza": round((sum([(x - sum(valores) / len(valores))**2 for x in valores]) / len(valores)),4)}
+
+#esto lo guarda como json
+with open('trabajos_practicos/datos.json', 'w') as jf: 
+    json.dump(frecuencias, jf, ensure_ascii=False, indent=2)
+
+# print(frecuencias)
     
-    # Calcular la frecuencia relativa de cada valo"
-    frecuencias["tirada: " + str(y)] = {"frecuencia relativa": valores.count(numero) / num_tiradas, 
-                           "frecuencia_absoluta": valores.count(numero)}
-
-
-print(frecuencias, "/n")
-    
-
-
-
-#     return frecuencia_absoluta, frecuencia_relativa
-
-# # Calcular frecuencias iniciales
-# frecuencia_absoluta, frecuencia_relativa = calcular_frecuencias(valores)
-
-# # Imprimir los resultados iniciales
-# print("Valores generados:", valores)
-# print("Frecuencia absoluta de 0:", frecuencia_absoluta[0])
-# print("Frecuencia absoluta de 1:", frecuencia_absoluta[1])
-# print("Frecuencia relativa de 0:", frecuencia_relativa[0])
-# print("Frecuencia relativa de 1:", frecuencia_relativa[1])
-
-# # Simular la adición de nuevos valores
-# while True:
-#     nuevo_valor = random.randint(0, 1)
-#     valores.append(nuevo_valor)
-#     frecuencia_absoluta, frecuencia_relativa = calcular_frecuencias(valores)
-#     print("\nNuevo valor generado:", nuevo_valor)
-#     print("Valores generados:", valores)
-#     print("Frecuencia absoluta de 0:", frecuencia_absoluta[0])
-#     print("Frecuencia absoluta de 1:", frecuencia_absoluta[1])
-#     print("Frecuencia relativa de 0:", frecuencia_relativa[0])
-#     print("Frecuencia relativa de 1:", frecuencia_relativa[1])
