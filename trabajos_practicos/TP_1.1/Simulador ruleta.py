c6178c7b933816d02ea07f4be2ee0feb3ec6c726
Gino Bartolucci <ginobartolucci00@gmail.com>
2024-03-29 05:36:32 -0300

    graficas de la primera corrida y graficas del promedio de 30 corridas
    se pueden optimizar las funciones

--- trabajos_practicos/TP_1.1/Simulador ruleta.py
+++ trabajos_practicos/TP_1.1/Simulador ruleta.py
@@ -7,83 +7,94 @@ parser = argparse.ArgumentParser(description="Simulador de ruleta")
 parser.add_argument("-c", default=37, type=int, help="Número de tiradas")
 parser.add_argument("-n", default=1, type=int, help="Número de corridas")
 parser.add_argument("-e", required=True, type=int, choices=range(0, 37), help="Número elegido")
-
 args = parser.parse_args()
 num_tiradas = args.c
 num_corridas = args.n
 numero = args.e
 
+def graficar (axs, x, y, c, titulo, xlabel, ylabel, color1, color2):
+    axs.plot(x, y, label='Valor obtenido',color=color1)
+    axs.plot(c, label='Valor esperado', linestyle='--', color=color2,)
+    axs.axis((0,num_tiradas ,0,c[0]*2))
+    axs.set_title(titulo)
+    axs.set_xlabel(xlabel)
+    axs.set_ylabel(ylabel)
+    axs.legend()
+
 corridas={}
-for y in range(num_corridas):
+for n in range(1, num_corridas + 1, 1):
     tiradas={}
     valores = []
     suma = 0
-    for c in range(1, num_tiradas+1, 1):
+    for c in range(1, num_tiradas + 1, 1):
         ruleta = random.randint(0, 37)        
         valores.append(ruleta)
-
         suma = suma + ruleta
-        fr = round((valores.count(numero) / c),4)
-        vp = round((suma / c),4)
-        vz = round((sum([(x - vp)**2 for x in valores]) / c),4)
-        de = round((vz**0.5),4)
-        tiradas[c] = {"frecuencia relativa": fr, "valor promedio": vp,
-                         "varianza": vz, "desviacion estandar": de}
-        
-    corridas[y] = tiradas
 
+        fr = valores.count(numero) / c
+        vp = suma / c
+        vz = sum([(x - vp)**2 for x in valores]) / c
+        de = vz**0.5
+        tiradas[c] = {"frecuencia relativa": fr, "valor promedio": vp,
+                      "varianza": vz, "desviacion estandar": de}
+    corridas[n] = tiradas
+#guardar json
 with open('trabajos_practicos/TP_1.1/datos.json', 'w') as jf: 
     json.dump(corridas, jf, ensure_ascii=False, indent=2)
 
+#Grafica primera corrida
+#valores en x
 x = list(range(num_tiradas))
-#valores en y
-y_fr = [corridas[0][i]['frecuencia relativa'] for i in range(1, num_tiradas+1, 1)]
-y_vp = [corridas[0][i]['valor promedio'] for i in range(1, num_tiradas+1, 1)]
-y_vz = [corridas[0][i]['varianza'] for i in range(1, num_tiradas+1, 1)]
-y_de = [corridas[0][i]['desviacion estandar'] for i in range(1, num_tiradas+1, 1)]
 #valores esperados
 c_fr = [1/37 for i in range(num_tiradas)]
 c_vp = [18 for i in range(num_tiradas)]
-c_vz = [324 for i in range(num_tiradas)]
-c_de = [18 for i in range(num_tiradas)]
-
-# Crear la figura y los subgráficos
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(18, 9))
-
-# Graficar frecuencia relativa
-axs[0][0].plot(x, y_fr, color='blue')
-axs[0][0].plot(c_fr, label='Valor esperado', linestyle='--', color='red')
-axs[0][0].set_title('Gráfico 1: Frecuencia relativa')
-axs[0][0].set_xlabel('Tiradas')
-axs[0][0].set_ylabel('Frecuencia relativa')
-
-# Graficar valor primedio
-axs[0][1].plot(x, y_vp, color='blue')
-axs[0][1].plot(c_vp, label='Valor esperado', linestyle='--', color='red')
-axs[0][1].set_title('Gráfico 2: Valor promedio')
-axs[0][1].set_xlabel('Tiradas')
-axs[0][1].set_ylabel('Valor promedio')
-
-# Graficar varianza
-axs[1][0].plot(x, y_vz, color='blue')
-axs[1][0].plot(c_vz, label='Valor esperado', linestyle='--', color='red')
-axs[1][0].set_title('Gráfico 3: Varianza')
-axs[1][0].set_xlabel('Tiradas')
-axs[1][0].set_ylabel('Varianza')
-
-# Graficar desvio estandar
-axs[1][1].plot(x, y_de, color='blue')
-axs[1][1].plot(c_de, label='Valor esperado', linestyle='--', color='red')
-axs[1][1].set_title('Gráfico 4: Desvio estandar')
-axs[1][1].set_xlabel('Tiradas')
-axs[1][1].set_ylabel('Desvio estandar')
-
-# Ajustar diseño y mostrar gráficos
+c_vz = [114 for i in range(num_tiradas)]
+c_de = [10.68 for i in range(num_tiradas)]
+#valores en y
+y_fr = [corridas[1][i]['frecuencia relativa'] for i in range(1, num_tiradas+1, 1)]
+y_vp = [corridas[1][i]['valor promedio'] for i in range(1, num_tiradas+1, 1)]
+y_vz = [corridas[1][i]['varianza'] for i in range(1, num_tiradas+1, 1)]
+y_de = [corridas[1][i]['desviacion estandar'] for i in range(1, num_tiradas+1, 1)]
+#graficar
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(14, 9))
+graficar(axs[0][0], x, y_fr, c_fr, 'Gráfico 1: Frecuencia relativa', 'Tiradas', 'Frecuencia relativa', 'blue', 'red')
+graficar(axs[0][1], x, y_vp, c_vp, 'Gráfico 2: Valor promedio', 'Tiradas', 'Valor promedio', 'blue', 'red')
+graficar(axs[1][0], x, y_vz, c_vz, 'Gráfico 3: Varianza', 'Tiradas', 'Varianza', 'blue', 'red')
+graficar(axs[1][1], x, y_de, c_de, 'Gráfico 4: Desvio estandar', 'Tiradas', 'Desvio estandar', 'blue', 'red')
 plt.tight_layout()
-
-# Guardar la figura en disco
 plt.savefig('trabajos_practicos/TP_1.1/cuatro_graficas_tp1.png')
-
-# Mostrar la figura
 plt.show()
-    
+
+#Grafica promedio de cada tirada
+avg = {}
+for c in range(1, num_tiradas + 1, 1):
+    frs = []
+    vps = []
+    vzs = []
+    des = []
+    for n in range(1, num_corridas + 1, 1):
+        frs.append(corridas[n][c]['frecuencia relativa'])
+        vps.append(corridas[n][c]['valor promedio'])
+        vzs.append(corridas[n][c]['varianza'])
+        des.append(corridas[n][c]['desviacion estandar'])
+    avg[c] = {"frecuencia relativa": (sum(frs) / num_corridas),
+                "valor promedio": (sum(vps) / num_corridas),
+                "varianza": (sum(vzs) / num_corridas),
+                "desviacion estandar": (sum(des) / num_corridas)} 
+#guardar json
+with open('trabajos_practicos/TP_1.1/datos_promedios.json', 'w') as jf: 
+    json.dump(avg, jf, ensure_ascii=False, indent=2)
+#valores en y (valores en x son los mismos)
+y_fr_avg = [avg[i]['frecuencia relativa'] for i in range(1, num_tiradas+1, 1)]
+y_vp_avg = [avg[i]['valor promedio'] for i in range(1, num_tiradas+1, 1)]
+y_vz_avg = [avg[i]['varianza'] for i in range(1, num_tiradas+1, 1)]
+y_de_avg = [avg[i]['desviacion estandar'] for i in range(1, num_tiradas+1, 1)]
+#graficar
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(14, 9))
+graficar(axs[0][0], x, y_fr_avg, c_fr, 'Gráfico 1: Frecuencia relativa', 'Tiradas', 'Frecuencia relativa', 'green', 'red')
+graficar(axs[0][1], x, y_vp_avg, c_vp, 'Gráfico 2: Valor promedio', 'Tiradas', 'Valor promedio', 'green', 'red')
+graficar(axs[1][0], x, y_vz_avg, c_vz, 'Gráfico 3: Varianza', 'Tiradas', 'Varianza', 'green', 'red')
+graficar(axs[1][1], x, y_de_avg, c_de, 'Gráfico 4: Desvio estandar', 'Tiradas', 'Desvio estandar', 'green', 'red')
+plt.tight_layout()
+plt.savefig('trabajos_practicos/TP_1.1/cuatro_graficas_avg_tp1.png')
+plt.show()
